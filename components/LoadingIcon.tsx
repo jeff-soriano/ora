7d96463e9f44682f5b368732823dc4ebfdbfea0b
Jeff Soriano <soriano.jeffm@gmail.com>
2025-05-31 21:54:13 -0700

    Refactor LoadingPage to separate out LoadingIcon for other use

---
+++ components/LoadingIcon.tsx
@@ -0,0 +1,9 @@
+import { FaCross } from "react-icons/fa";
+
+export default function LoadingIcon() {
+  return (
+    <div className="text-gray-500 animate-pulse">
+      <FaCross size={50} />
+    </div>
+  );
+}
--- components/LoadingPage.tsx
+++ components/LoadingPage.tsx
@@ -1,9 +1,9 @@
-import { FaCross } from "react-icons/fa";
+import LoadingIcon from "./LoadingIcon";
 
 export default function LoadingPage() {
   return (
-    <div className="min-h-[calc(100vh-64px)] flex flex-col items-center justify-center bg-[#f8f5ef] px-4 py-8 text-gray-500 animate-pulse">
-      <FaCross size={50} />
+    <div className="min-h-[calc(100vh-64px)] bg-[#f8f5ef] flex flex-col items-center justify-center px-4 py-8 ">
+      <LoadingIcon />
     </div>
   );
 }
