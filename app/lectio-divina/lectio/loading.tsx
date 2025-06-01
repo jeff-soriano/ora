bc7d22d15c995edcb30f2e1fa6db02f6668c3251
Jeff Soriano <soriano.jeffm@gmail.com>
2025-05-31 20:02:09 -0700

    Add and use LoadingPage for lectio

--- app/lectio-divina/lectio/loading.tsx
+++ app/lectio-divina/lectio/loading.tsx
@@ -1,9 +1,5 @@
-import { FaCross } from "react-icons/fa";
+import LoadingPage from "@/components/LoadingPage";
 
 export default function LectioLoading() {
-  return (
-    <div className="min-h-[calc(100vh-64px)] flex flex-col items-center justify-center bg-[#f8f5ef] px-4 py-8 text-gray-500 animate-pulse">
-      <FaCross size={50} />
-    </div>
-  );
+  return <LoadingPage />;
 }
---
+++ components/LoadingPage.tsx
@@ -0,0 +1,9 @@
+import { FaCross } from "react-icons/fa";
+
+export default function LoadingPage() {
+  return (
+    <div className="min-h-[calc(100vh-64px)] flex flex-col items-center justify-center bg-[#f8f5ef] px-4 py-8 text-gray-500 animate-pulse">
+      <FaCross size={50} />
+    </div>
+  );
+}
