f1dfbe43a01b1744ed1e20f773280105cd2f6ba1
Jeff Soriano <soriano.jeffm@gmail.com>
2025-06-05 19:42:19 -0700

    Use AIInsightPrompt for completion page of lectio divina

--- app/lectio-divina/complete/GenerateLectioDivinaInsight.tsx
+++ app/lectio-divina/complete/GenerateLectioDivinaInsight.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import LoadingIcon from "@/components/LoadingIcon";
 import { generateLectioDivinaInsight, fetchBibleVerse } from "@/lib/openai";
 import AIReflectionBlock from "@/components/AIReflectionBlock";
+import AIInsightPrompt from "@/components/AIInsightPrompt";
 
 export default function GenerateLectioDivinaInsight({
   lectioReflection,
@@ -53,29 +54,15 @@ export default function GenerateLectioDivinaInsight({
   }
 
   return (
-    <div className="max-w-xl mx-auto p-6 bg-[#f8f5ef] border border-black rounded-2xl shadow-sm">
-      <div className="flex items-center mb-2">
-        <span className="text-2xl mr-2">💡</span>
-        <h2 className="text-xl md:text-3xl font-serif font-bold">
-          Want to go deeper?
-        </h2>
-      </div>
-      <p className="text-lg md:text-xl mb-6 text-gray-900">
-        Let Ora gently reflect on your prayer and offer spiritual insight — like
-        a spiritual director, but digital.
-      </p>
-      <button
-        onClick={handleClick}
-        className="w-full bg-black text-white md:text-lg font-semibold rounded-lg py-3 mb-2 flex items-center justify-center hover:bg-gray-900 transition-colors cursor-pointer"
-      >
-        Generate Spiritual Insight <span className="ml-2">&rarr;</span>
-      </button>
-      <p className="text-sm text-gray-900 italic">
-        By clicking above, you agree to share your prayer reflections with a
+    <AIInsightPrompt
+      onClick={handleClick}
+      header="Want to go deeper?"
+      body="Let Ora gently reflect on your prayer and offer spiritual insight — like
+        a spiritual director, but digital."
+      disclaimer="By clicking above, you agree to share your prayer reflections with a
         trusted third-party AI (OpenAI) to generate spiritual insight. Your
-        responses won’t be stored or reused. This is optional and meant to
-        gently support your prayer life.
-      </p>
-    </div>
+        responses won’t be stored or reused outside this device. This is optional and meant to
+        gently support your prayer life."
+    />
   );
 }
